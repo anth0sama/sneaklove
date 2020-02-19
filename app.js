cab0d65f393f4bae7edf2111892493a45a927a51
Jepassaispar <51320916+Jepassaispar@users.noreply.github.com>
2020-02-19 18:55:39 +0100

    sneakLove debugged

--- app.js
+++ app.js
@@ -1,62 +1,86 @@
+require("dotenv").config();
+require("./config/mongodb"); // database initial setup
+require("./helpers/helpers-hbs"); // utils for hbs templates
+
+// base dependencies
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
 const session = require("express-session");
 const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser")
-require("dotenv").config();
-require("./config/mongodb"); // database initial setup
-require("./helpers/helpers-hbs"); 
+const MongoStore = require("connect-mongo")(session);
+const cookieParser = require("cookie-parser");
 
-app.set("views", __dirname + "/view");
+// initial config
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
 app.use(express.static("public"));
-hbs.registerPartials(__dirname + "/views/partials");
-app.use(express.urlencoded({extended: false}));
-app.use(express.urlencoded({
-  extended: false
-}));
+hbs.registerPartials(__dirname + "/views/partial");
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-
+// SESSION SETUP
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     cookie: {
       maxAge: 60000
-    },
+    }, // in millisec
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    }),
     saveUninitialized: true,
     resave: true
-  }
-));
+  })
+);
 
+app.locals.site_url = process.env.SITE_URL;
+// used in front end to perform ajax request (var instead of hardcoded)
+
+// CUSTOM MIDDLEWARE
+// check if user is logged in...
+// usecases : conditional display in hbs templates
+// WARNING: this function must be declared AFTER the session setup
+// WARNING: this function must be declared BEFORE app.use(router(s))
 function checkloginStatus(req, res, next) {
-  res.locals.user = req.session.currentUser ? req.session.currentUser : null;
+  // res.locals.user === req.session.currentUser ? req.session.currentUser : null;
+  // access this value @ {{user}} or {{user.prop}} in .hbs
   res.locals.isLoggedIn = Boolean(req.session.currentUser);
-} 
-      function eraseSessionMessage() {
-        var count = 0;
-        return function (req, res, next) {
-          if (req.session.msg) { 
-            if (count) { 
-              count = 0; 
-              console.log(`app started at ${process.env.SITE_URL}:${process.env.PORT}`);
-            }
-            ++count;
-          }
-          next();
-        }
+  // access this value @ {{isLoggedIn}} in .hbs
+  next(); // continue to the requested route
+}
+
+function eraseSessionMessage() {
+  var count = 0; // initialize counter in parent scope and use it in inner function
+  return function(req, res, next) {
+    if (req.session.msg) {
+      // only increment if session contains msg
+      if (count) {
+        // if count greater than 0
+        count = 0; // reset counter
+        req.session.msg = null; // reset message
       }
+      ++count; // increment counter
+    }
+    next(); // continue to the requested route
+  };
+}
 
+app.use(checkloginStatus);
+app.use(eraseSessionMessage());
 
-app.use(checkloginStatus)
-app.use(eraseSessionMessage())
+// Getting/Using router(s)
+const index = require("./routes/index");
+app.use("/", index);
 
-const index = require('./routes/index')
-app.use("/", index)
-
-const listener = app.listen(process.env.PORT, () => {
-  console.log(`app is running at ${process.env.SITE_URL}:${process.env.PORT}`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`app started at ${process.env.SITE_URL}:${process.env.PORT}`);
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -7,39 +7,41 @@ router.get(["/", "/index"], (req, res) => {
 });
 
 router.get("/sneakers/collection", (req, res, next) => {
-  sneakerModel.find()
-    .populate('id_tags')
+  sneakerModel
+    .find()
+    .populate("id_tags")
     .then(sneakers => {
       res.render("products", {
         sneakers
       });
     })
-    .catch(dbErr => next(dbErr))
+    .catch(dbErr => next(dbErr));
 });
 
 router.get("/sneakers/:cat", (req, res, next) => {
-  console.log(req.params.cat)
-  sneakerModel.find({
+  console.log(req.params.cat);
+  sneakerModel
+    .find({
       category: req.params.cat
     })
-    .populate('id_tags')
+    .populate("id_tags")
     .then(sneakers => {
       res.render("products", {
         sneakers
       });
     })
-    .catch(dbErr => next(dbErr))
+    .catch(dbErr => next(dbErr));
 });
 
 router.get("/one-product/:id", (req, res, next) => {
-  sneakerModel.findById(req.params.id)
+  sneakerModel
+    .findById(req.params.id)
     .then(sneaker => {
       res.render("one_product", {
         sneaker
       });
     })
-    .catch(dbErr => next(dbErr))
+    .catch(dbErr => next(dbErr));
 });
-
 
 module.exports = router;
